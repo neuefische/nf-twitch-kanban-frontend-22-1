441d390485908840c80799afb82bf274e639935e
Droggelbecher92 <thomaskittlaus@gmail.com>
2022-06-16 11:14:40 +0200

    template build

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,33 @@ import React from 'react';
 import './App.css';
 
 export default function App() {
-  return (
-    <div className="App">
-      Hallo
-    </div>
-  );
+
+    const columns = ["TODO", "IN PROGRESS", "DONE"]
+
+    return (
+        <div className="App">
+            <h1>Kanban-Board</h1>
+            <div>
+                <input type="text"/>
+                <input type="text"/>
+                <button>save</button>
+            </div>
+            {
+                columns.map(column =>
+                    <div key={column}>
+                        <h2>{column}</h2>
+                        <div>
+                            <p>Hunde füttern</p>
+                            <p>Futter einkaufen</p>
+                            <button>prev</button>
+                            <button>edit</button>
+                            <button>next</button>
+                        </div>
+                    </div>
+                )
+            }
+        </div>
+    );
 }
 
 
